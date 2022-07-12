b50bab5ad33feea09fafd148a918affacc00b89e
anabeatriz <thezolin00@gmail.com>
2022-07-12 18:20:02 -0300

    Atualização Repeat-DNA

--- Repeat-DNA.cpp
+++ Repeat-DNA.cpp
@@ -10,13 +10,6 @@ KSEQ_INIT(gzFile, gzread);
 
 using namespace std;
 
-/////// sequence struct ///////
-typedef struct sequence{
-	int n;
-	char *T;
-	struct sequence *prox;
-} s_seq;
-
 /////// open files SA and LCP ///////
 int open(char *arq, int n, int *ARQ){
     FILE* f = fopen(arq, "rb"); // rb to binary files
@@ -46,33 +39,42 @@ char print(int k, char *file_type, char *arg, char *t){
     return *file_type;
 }
 
-/////// read fasta and fastq files ///////
-long int read_fast(s_seq *seq_sq, char *arg){
+/////// file size ///////
+long int sum_fast(long int sum, char *arg){
     gzFile fp;
     kseq_t *seq;
-    s_seq *sq = (s_seq*)malloc(sizeof(s_seq));
+    fp = gzopen(arg, "r");
+    seq = kseq_init(fp);
+    int n;
+    while ((n = kseq_read(seq)) >= 0){
+        if(seq->seq.s) sum += n+1;  
+    }
+    fp = gzopen(arg, "r");
+    seq = kseq_init(fp);
+    kseq_destroy(seq);
+    gzclose(fp);
+    return sum; // total concatenation size
+}
+
+/////// read fasta and fastq files ///////
+char read_fast(long int sum, char *arg, char *T){
+    gzFile fp;
+    kseq_t *seq;
 
     fp = gzopen(arg, "r");
     seq = kseq_init(fp);
     int n;
-    long int sum = 0;
     while ((n = kseq_read(seq)) >= 0){
-        sum += n;
-        sq->T = seq->seq.s;
-        sq->n = sum;
+        if(seq->seq.s){
+            strcat(T, seq->seq.s);
+            strcat(T, "$");
+        }
     }
+    T[sum-1] = '#';
     fp = gzopen(arg, "r");
     seq = kseq_init(fp);
-    return sum; // total concatenation size
-}
-
-char seq_fast(char *T, long int n, s_seq *seq_sq){
-    s_seq *aux = (s_seq*)malloc(sizeof(s_seq));
-    aux = seq_sq;printf("entrou\n\n");
-    while(aux->prox == NULL){
-        printf("-%s\n", aux->T);
-        strcat(T, aux->T);
-    }
+    kseq_destroy(seq);
+    gzclose(fp);
     return *T;
 }
 
@@ -87,16 +89,16 @@ int main(int argc, char *argv[]){
     int op_p = atoi(argv[7]);
     int op_file = atoi(argv[8]);
     
-    long int n;
+    char *file_out = argv[9];
+    long int n = 0;
 
-    s_seq *seq_sq = NULL;
-    char *T;
+    char *T = NULL;
 
     /////// read txt files ///////
     if(op_file == 0){
         FILE* f = fopen(argv[1], "r"); 
         fseek(f, 0, SEEK_END);
-        n = ftell(f); 
+        n = ftell(f) + 1; 
         rewind(f);
         T = (char*)malloc(sizeof(char)*(n+1));
         int ret = fread(T, sizeof(char), n, f);
@@ -108,19 +110,19 @@ int main(int argc, char *argv[]){
     }
     /////// read fasta and fastq files ///////
     else if(op_file == 1){
-        n = read_fast(seq_sq, argv[1]);
+        n = sum_fast(n, argv[1]) + 1;
         T = (char*)malloc(sizeof(char)*(n+1));
-        //*T = seq_fast(T, n, seq_sq);
+        *T = read_fast(n, argv[1], T);
     }
 
     FILE *fi = fopen(argv[2], "rb"); // rb to binary files
     char *BWT = (char*)malloc(sizeof(char)*(n+1));
     int retn = fread(BWT, sizeof(char), n, fi);
-    if(retn != n){ // não pega o arquivo inteirooo
+    if(retn != n){
         printf("Error reading file %s\n", argv[2]);
     }
     fclose(fi);
-    printf("%s\n", BWT);
+    printf("\n%s\n", BWT);
 
     int *SA = (int*)malloc(sizeof(int)*(n+1));
     open(argv[3], n, SA);
@@ -151,7 +153,7 @@ int main(int argc, char *argv[]){
     /////// if repeat ir type 1 or both ///////
     if(op == 1 || op == 0){
         char *type1 = (char*)".type1";
-        *file_type = print(k, file_type, argv[1], type1);
+        *file_type = print(k, file_type, file_out, type1);
         fi = fopen(file_type, "wb");
         
         for(int i = 0; i <= n; i++){
@@ -206,9 +208,9 @@ int main(int argc, char *argv[]){
     if(op == 2 || op == 0){
 
         char *type2 = (char*)".type2";
-        *file_type = print(k, file_type, argv[1], type2);
+        *file_type = print(k, file_type, file_out, type2);
         fi = fopen(file_type, "wb");
-
+        
         for(int i = 0; i <= n; i++){
             if(LCP[i] == l_input && LCP[i-1] < l_input){
                 a.push(i-1);
